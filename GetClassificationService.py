e270e3a5343e2ba499dc7434fb2f409994b6ba17
Vckarp <vckarp@wp.pl>
2018-01-16 17:49:21 +0100

    Added database functionality to the ModelTrainService. GetClassificationService now has a function to post json to the ModelTrainService

--- GetClassificationService.py
+++ GetClassificationService.py
@@ -1,18 +1,28 @@
 from flask import Flask
 import pandas as pd
 import pickle
-
+import json
 import requests
 
-app = Flask("GetClassification")
+app = Flask(__name__)
 
 
 @app.route("/classify", methods=['GET'])
 def classify():
     clf = pickle.loads(requests.get('http://127.0.0.1:5000/get_clf').content)
     data = pd.read_csv("sample_data.csv", header=None)
+    data = data.iloc[:, :-1]
     pred = clf.predict(data)
     return "\n\n".join(pred)
+
+
+@app.route("/post_new")
+def post_new_data():
+    """A function to check the functionality of the append_db function from the ModelTrainService.py"""
+    with open('sample_data.json') as f:
+        data = json.load(f)
+    request = requests.post('http://127.0.0.1:5000/append_db', json=data)
+    return request.content
 
 
 if __name__ == '__main__':
--- ModelTrainService.py
+++ ModelTrainService.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, make_response
-import json
+from flask import Flask, request
 import pandas as pd
 import pickle
 from sklearn import svm
 import os
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 
 def string_to_int(data):
@@ -20,15 +21,31 @@ def string_to_int(data):
     return data
 
 
-app = Flask("train_model")
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+
+class Dataset(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    feature1 = db.Column(db.Integer)
+    feature2 = db.Column(db.Integer)
+    feature3 = db.Column(db.Integer)
+    feature4 = db.Column(db.Integer)
+    feature5 = db.Column(db.Integer)
+    feature6 = db.Column(db.Integer)
+    target = db.Column(db.String(20))
+
+    def __repr__(self):
+        return 'Row_No: {}  Features: {} {} {} {} {} {} Target: {} ||||||| '.format(self.id, self.feature1, self.feature2, self.feature3,
+                                                                        self.feature4, self.feature5, self.feature6, self.target)
 
 
 @app.route("/train", methods=['GET'])
 def train_model():
-    data = pd.read_csv('carData.txt', header=None)
+    data = pd.read_sql_query('select * from Dataset', Dataset.query.session.bind)
     target = data.iloc[:, -1]
-    data = data.iloc[:, :-1]
-    data = string_to_int(data)
+    data = data.iloc[:, 1:-1]
     clf2 = svm.SVC()
     # print(clf2.fit(X=data.values, y=target.values).score(data, target))
     clf2.fit(X=data.values, y=target.values)
@@ -37,9 +54,25 @@ def train_model():
     return str(clf2.score(data, target))
 
 
-# @app.route("/train", methods=["POST"])
-# def append_and_retrain():
-#     with open("")
+@app.route("/append_db", methods=["POST"])
+def append_db():
+    try:
+        new_data = request.get_json()
+    except Exception:
+        return 'Error'
+    if isinstance(new_data, list):
+        for row in new_data:
+            keys = list(row.keys())
+            db.session.add(Dataset(feature1=row[keys[0]], feature2=row[keys[1]], feature3=row[keys[2]], feature4=row[keys[3]],
+                                   feature5=row[keys[4]], feature6=row[keys[5]], target=row[keys[6]]))
+        db.session.commit()
+    if isinstance(new_data, dict):
+        keys = list(new_data.keys())
+        db.session.add(Dataset(feature1=new_data[keys[0]], feature2=new_data[keys[1]], feature3=new_data[keys[2]],
+                               feature4=new_data[keys[3]], feature5=new_data[keys[4]], feature6=new_data[keys[5]],
+                               target=new_data[keys[6]]))
+        db.session.commit()
+    return "Rows added to the table, check localhost:5000/show_data to see if the new rows are appended to the db"
 
 
 @app.route("/get_clf", methods=['GET'])
@@ -50,6 +83,27 @@ def provide_clf():
         return pickle.dumps(response, protocol=pickle.HIGHEST_PROTOCOL)
     else:
         return "No classifier found. Perhaps it should be recreated."
+
+
+@app.route("/export_data")
+def export_data():
+    data = pd.read_csv('carData.txt', header=None)
+    target = data.iloc[:, -1]
+    data = data.iloc[:, :-1]
+    data = string_to_int(data)
+    data = pd.concat([data.reset_index(), target], axis=1)
+    for row in data.values:
+        db.session.add(Dataset(feature1=row[1], feature2=row[2], feature3=row[3], feature4=row[4],
+                               feature5=row[5], feature6=row[6], target=row[7]))
+    db.session.commit()
+
+
+@app.route("/show_data")
+def show_data():
+    data = Dataset.query.all()
+    data = [row.__str__() for row in data]
+    data = "".join(data)
+    return data
 
 
 if __name__ == '__main__':
